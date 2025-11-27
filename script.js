38f40f2cb749d3048af3f394ff2da50782fa7ee5
Eugene Fokin <evgen94@bk.ru>
2025-11-27 20:25:09 +0300

    reload new all

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
+  // 1. КНОПКА НАВЕРХ
   var btnUp = $('.footer-up');
-
+  
   $(window).scroll(function() {
     if ($(this).scrollTop() > 300) {
-      btnUp.fadeIn();
+      btnUp.fadeIn(300);
     } else {
-      btnUp.fadeOut();
+      btnUp.fadeOut(300);
+    }
+  });
+  
+  btnUp.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 800, 'swing');
+  });
+
+  // 2. ПЛАВНЫЙ СКРОЛЛ ПО ЯКОРЯМ МЕНЮ 
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - 90
+      }, 800);
     }
   });
 
-  btnUp.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop: 0}, 'slow');
+  // 3. АКТИВНОЕ МЕНЮ ПРИ ПРОКРУТКЕ
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    $('.section, .contacts, .intro').each(function() {
+      var top = $(this).offset().top - 100;
+      if (scroll >= top) {
+        $('.menu li').removeClass('active');
+        $('.menu a[href="#' + $(this).attr('id') + '"]')
+          .parent().addClass('active');
+      }
+    });
   });
 });
